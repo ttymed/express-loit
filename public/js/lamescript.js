7d328a206856db19e7ec08dd05fd17ff3fda4e94
Tyler Boright <WuKong@Tylers-MacBook-Air.local>
2014-03-11 16:07:36 -1000

    added some regex protection

--- public/js/lamescript.js
+++ public/js/lamescript.js
@@ -6,6 +6,36 @@ $(function(){
       alert("You must enter your name and email.");
       e.preventDefault();
     }
+    function validateComment(){
+    var Code = document.getElementById('comment').value;
+
+    if( /[^a-zA-Z0-9\-\/]/.test( TCode ) ) {
+        alert('Input is not alphanumeric');
+        return false;
+    }
+
+    return true;     
+}
+	function validateName(){
+    var NCode = document.getElementById('name').value;
+
+    if( /[^a-zA-Z0-9\-\/]/.test( TCode ) ) {
+        alert('Input is not alphanumeric');
+        return false;
+    }
+
+    return true;     
+}
+	function validateEmail() {
+		var eCode = document.getElementById('email').value;
+
+		if( /[^a-zA-Z0-9\-\/]/.test( TCode ) ) {
+        alert('Input is not alphanumeric');
+        return false;
+    }
+
+    return true;     
+}
   });
 
 });
