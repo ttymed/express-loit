ac33fe33009e30349413db2b111240e4a2760991
Tyler Boright <WuKong@Tylers-MacBook-Air.local>
2014-03-11 16:22:39 -1000

    called functions in submit click instead of just invoking them

--- public/js/lamescript.js
+++ public/js/lamescript.js
@@ -5,37 +5,25 @@ $(function(){
     if($('#name').val().trim() == "" || $('#email').val().trim() == ""){
       alert("You must enter your name and email.");
       e.preventDefault();
-    }
-    function validateComment(){
-    var Code = document.getElementById('comment').value;
+    	}
+    
+	    var Code = document.getElementById('comment').value;
 
-    if( /[^a-zA-Z0-9\-\/]/.test( TCode ) ) {
-        alert('Input is not alphanumeric');
-        return false;
-    }
+	    if( /[^a-zA-Z0-9\-\/]/.test( TCode ) ) {
+	        alert('Input is not alphanumeric');
+		}
+	
+	    var NCode = document.getElementById('name').value;
 
-    return true;     
-}
-	function validateName(){
-    var NCode = document.getElementById('name').value;
-
-    if( /[^a-zA-Z0-9\-\/]/.test( TCode ) ) {
-        alert('Input is not alphanumeric');
-        return false;
-    }
-
-    return true;     
-}
-	function validateEmail() {
+	    if( /[^a-zA-Z0-9\-\/]/.test( TCode ) ) {
+	        alert('Input is not alphanumeric');
+	
+		}
 		var eCode = document.getElementById('email').value;
 
 		if( /[^a-zA-Z0-9\-\/]/.test( TCode ) ) {
-        alert('Input is not alphanumeric');
-        return false;
-    }
-
-    return true;     
-}
+        alert('Input is not alphanumeric');     
+		}
   });
 
 });
